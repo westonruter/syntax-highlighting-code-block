28aafe12b8925cfa1fbde33e2ea0b4cf53319d28
Weston Ruter <westonruter@google.com>
2020-04-21 11:45:38 -0700

    Fix compatibility with lightBlockWrapper for Code block in Gutenberg 7.9

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ import { addFilter } from '@wordpress/hooks';
 import { PlainText, InspectorControls } from '@wordpress/editor';
 import { SelectControl, CheckboxControl, PanelBody, PanelRow } from '@wordpress/components';
 import { Fragment } from '@wordpress/element';
+import * as BlockEditor from '@wordpress/block-editor';
 
 /**
  * Internal dependencies
@@ -27,6 +28,8 @@ const extendCodeBlockWithSyntaxHighlighting = ( settings ) => {
 	if ( 'core/code' !== settings.name ) {
 		return settings;
 	}
+
+	const useLightBlockWrapper = settings.supports && settings.supports.lightBlockWrapper && BlockEditor.__experimentalBlock && BlockEditor.__experimentalBlock.pre;
 
 	return {
 		...settings,
@@ -83,14 +86,28 @@ const extendCodeBlockWithSyntaxHighlighting = ( settings ) => {
 						</PanelRow>
 					</PanelBody>
 				</InspectorControls>
-				<div key="editor-wrapper" className={ className }>
-					<PlainText
-						value={ attributes.content }
-						onChange={ ( content ) => setAttributes( { content } ) }
-						placeholder={ __( 'Write code…', 'syntax-highlighting-code-block' ) }
-						aria-label={ __( 'Code', 'syntax-highlighting-code-block' ) }
-					/>
-				</div>
+				{
+					useLightBlockWrapper ?
+						// This must be kept in sync with <https://github.com/WordPress/gutenberg/blob/master/packages/block-library/src/code/edit.js>.
+						<BlockEditor.__experimentalBlock.pre>
+							<PlainText
+								__experimentalVersion={ 2 }
+								tagName="code"
+								value={ attributes.content }
+								onChange={ ( content ) => setAttributes( { content } ) }
+								placeholder={ __( 'Write code…', 'syntax-highlighting-code-block' ) }
+								aria-label={ __( 'Code', 'syntax-highlighting-code-block' ) }
+							/>
+						</BlockEditor.__experimentalBlock.pre> :
+						<div key="editor-wrapper" className={ className }>
+							<PlainText
+								value={ attributes.content }
+								onChange={ ( content ) => setAttributes( { content } ) }
+								placeholder={ __( 'Write code…', 'syntax-highlighting-code-block' ) }
+								aria-label={ __( 'Code', 'syntax-highlighting-code-block' ) }
+							/>
+						</div>
+				}
 			</Fragment>;
 		},
 
