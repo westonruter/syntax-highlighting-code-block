5ae1e3ee7842855406eb90e3a0401d8d74a39f4c
Weston Ruter <westonruter@google.com>
2020-04-25 17:11:16 -0700

    Fix prettier issues in index.js

--- src/index.js
+++ src/index.js
@@ -31,96 +31,94 @@ import languagesNames from './language-names';
  * @param {Object} settings Settings.
  * @return {Object} Modified settings.
  */
-const extendCodeBlockWithSyntaxHighlighting = ( settings ) => {
-	if ( 'core/code' !== settings.name ) {
+const extendCodeBlockWithSyntaxHighlighting = (settings) => {
+	if ('core/code' !== settings.name) {
 		return settings;
 	}
 
 	const useLightBlockWrapper =
-		hasBlockSupport( settings, 'core/code', 'lightBlockWrapper', false ) &&
+		hasBlockSupport(settings, 'core/code', 'lightBlockWrapper', false) &&
 		BlockEditor.__experimentalBlock &&
 		BlockEditor.__experimentalBlock.pre;
 
-	const HighlightablePlainText = ( props_ ) => {
+	const HighlightablePlainText = (props_) => {
 		const { highlightedLines, ...props } = props_;
 		const plainTextRef = createRef();
-		const [ styles, setStyles ] = useState( {} );
+		const [styles, setStyles] = useState({});
 
-		useEffect( () => {
-			if ( plainTextRef.current !== null ) {
+		useEffect(() => {
+			if (plainTextRef.current !== null) {
 				let element = plainTextRef.current;
 
 				// In Gutenberg 7.8 and below, the DOM element was stored in a property with the name of the node type.
 				// In 7.9+, the DOM element is now stored in `current`. This block is here for backward-compatibility
 				// with older Gutenberg versions.
-				if ( element.hasOwnProperty( 'textarea' ) ) {
+				if (element.hasOwnProperty('textarea')) {
 					element = plainTextRef.current.textarea;
 				}
 
-				const computedStyles = window.getComputedStyle( element );
+				const computedStyles = window.getComputedStyle(element);
 
-				setStyles( {
-					fontFamily: computedStyles.getPropertyValue(
-						'font-family'
-					),
-					fontSize: computedStyles.getPropertyValue( 'font-size' ),
-					overflow: computedStyles.getPropertyValue( 'overflow' ),
+				setStyles({
+					fontFamily: computedStyles.getPropertyValue('font-family'),
+					fontSize: computedStyles.getPropertyValue('font-size'),
+					overflow: computedStyles.getPropertyValue('overflow'),
 					overflowWrap: computedStyles.getPropertyValue(
 						'overflow-wrap'
 					),
-					resize: computedStyles.getPropertyValue( 'resize' ),
-				} );
+					resize: computedStyles.getPropertyValue('resize'),
+				});
 			}
-		}, [] );
+		}, []);
 
 		return (
 			<Fragment>
-				<PlainText ref={ plainTextRef } { ...props } />
+				<PlainText ref={plainTextRef} {...props} />
 				<div
-					aria-hidden={ true }
+					aria-hidden={true}
 					className="code-block-overlay"
-					style={ styles }
+					style={styles}
 				>
-					{ props.value.split( /\n/ ).map( ( v, i ) => {
+					{props.value.split(/\n/).map((v, i) => {
 						let cName = 'loc';
 
-						if ( highlightedLines.has( i ) ) {
+						if (highlightedLines.has(i)) {
 							cName += ' highlighted';
 						}
 
 						return (
-							<span key={ i } className={ cName }>
-								{ v || ' ' }
+							<span key={i} className={cName}>
+								{v || ' '}
 							</span>
 						);
-					} ) }
+					})}
 				</div>
 			</Fragment>
 		);
 	};
 
-	const parseSelectedLines = ( selectedLines ) => {
+	const parseSelectedLines = (selectedLines) => {
 		const highlightedLines = new Set();
 
-		if ( ! selectedLines || selectedLines.trim().length === 0 ) {
+		if (!selectedLines || selectedLines.trim().length === 0) {
 			return highlightedLines;
 		}
 
 		let chunk;
-		const ranges = selectedLines.replace( /\s/, '' ).split( ',' );
+		const ranges = selectedLines.replace(/\s/, '').split(',');
 
-		for ( chunk of ranges ) {
-			if ( chunk.indexOf( '-' ) >= 0 ) {
+		for (chunk of ranges) {
+			if (chunk.indexOf('-') >= 0) {
 				let i;
-				const range = chunk.split( '-' );
+				const range = chunk.split('-');
 
-				if ( range.length === 2 ) {
-					for ( i = +range[ 0 ]; i <= +range[ 1 ]; ++i ) {
-						highlightedLines.add( i - 1 );
+				if (range.length === 2) {
+					for (i = +range[0]; i <= +range[1]; ++i) {
+						highlightedLines.add(i - 1);
 					}
 				}
 			} else {
-				highlightedLines.add( +chunk - 1 );
+				highlightedLines.add(+chunk - 1);
 			}
 		}
 
@@ -146,118 +144,117 @@ const extendCodeBlockWithSyntaxHighlighting = ( settings ) => {
 			},
 		},
 
-		edit( { attributes, setAttributes, className } ) {
-			const updateLanguage = ( language ) => {
-				setAttributes( { language } );
+		edit({ attributes, setAttributes, className }) {
+			const updateLanguage = (language) => {
+				setAttributes({ language });
 			};
 
-			const updateSelectedLines = ( selectedLines ) => {
-				setAttributes( { selectedLines } );
+			const updateSelectedLines = (selectedLines) => {
+				setAttributes({ selectedLines });
 			};
 
-			const updateShowLines = ( showLines ) => {
-				setAttributes( { showLines } );
+			const updateShowLines = (showLines) => {
+				setAttributes({ showLines });
 			};
 
-			const updateWrapLines = ( wrapLines ) => {
-				setAttributes( { wrapLines } );
+			const updateWrapLines = (wrapLines) => {
+				setAttributes({ wrapLines });
 			};
 
 			const sortedLanguageNames = sortBy(
-				Object.entries(
-					languagesNames
-				).map( ( [ value, label ] ) => ( { label, value } ) ),
-				( languageOption ) => languageOption.label.toLowerCase()
+				Object.entries(languagesNames).map(([value, label]) => ({
+					label,
+					value,
+				})),
+				(languageOption) => languageOption.label.toLowerCase()
 			);
 
 			const plainTextProps = {
 				value: attributes.content || '',
-				highlightedLines: parseSelectedLines(
-					attributes.selectedLines
-				),
-				onChange: ( content ) => setAttributes( { content } ),
-				placeholder: __( 'Write code…' ),
-				'aria-label': __( 'Code' ),
+				highlightedLines: parseSelectedLines(attributes.selectedLines),
+				onChange: (content) => setAttributes({ content }),
+				placeholder: __('Write code…'),
+				'aria-label': __('Code'),
 				className: [
 					'shcb-plain-text',
 					attributes.wrapLines ? 'shcb-plain-text-wrap-lines' : '',
-				].join( ' ' ),
+				].join(' '),
 			};
 
 			return (
 				<Fragment>
 					<InspectorControls key="controls">
 						<PanelBody
-							title={ __( 'Syntax Highlighting' ) }
-							initialOpen={ true }
+							title={__('Syntax Highlighting')}
+							initialOpen={true}
 						>
 							<PanelRow>
 								<SelectControl
-									label={ __( 'Language' ) }
-									value={ attributes.language }
-									options={ [
+									label={__('Language')}
+									value={attributes.language}
+									options={[
 										{
-											label: __( 'Auto-detect' ),
+											label: __('Auto-detect'),
 											value: '',
 										},
 										...sortedLanguageNames,
-									] }
-									onChange={ updateLanguage }
+									]}
+									onChange={updateLanguage}
 								/>
 							</PanelRow>
 							<PanelRow>
 								<TextControl
-									label={ __( 'Highlighted Lines' ) }
-									value={ attributes.selectedLines }
-									onChange={ updateSelectedLines }
-									help={ __( 'Supported format: 1, 3-5' ) }
+									label={__('Highlighted Lines')}
+									value={attributes.selectedLines}
+									onChange={updateSelectedLines}
+									help={__('Supported format: 1, 3-5')}
 								/>
 							</PanelRow>
 							<PanelRow>
 								<CheckboxControl
-									label={ __( 'Show Line Numbers' ) }
-									checked={ attributes.showLines }
-									onChange={ updateShowLines }
+									label={__('Show Line Numbers')}
+									checked={attributes.showLines}
+									onChange={updateShowLines}
 								/>
 							</PanelRow>
 							<PanelRow>
 								<CheckboxControl
-									label={ __( 'Wrap Lines' ) }
-									checked={ attributes.wrapLines }
-									onChange={ updateWrapLines }
+									label={__('Wrap Lines')}
+									checked={attributes.wrapLines}
+									onChange={updateWrapLines}
 								/>
 							</PanelRow>
 						</PanelBody>
 					</InspectorControls>
-					{ useLightBlockWrapper ? (
+					{useLightBlockWrapper ? (
 						// This must be kept in sync with <https://github.com/WordPress/gutenberg/blob/master/packages/block-library/src/code/edit.js>.
 						<BlockEditor.__experimentalBlock.pre>
 							<HighlightablePlainText
-								{ ...plainTextProps }
-								__experimentalVersion={ 2 }
+								{...plainTextProps}
+								__experimentalVersion={2}
 								tagName="code"
 							/>
 						</BlockEditor.__experimentalBlock.pre>
 					) : (
-						<div key="editor-wrapper" className={ className }>
-							<HighlightablePlainText { ...plainTextProps } />
+						<div key="editor-wrapper" className={className}>
+							<HighlightablePlainText {...plainTextProps} />
 						</div>
-					) }
+					)}
 				</Fragment>
 			);
 		},
 
-		save( { attributes } ) {
+		save({ attributes }) {
 			return (
 				<pre>
-					<code>{ attributes.content }</code>
+					<code>{attributes.content}</code>
 				</pre>
 			);
 		},
 
 		// Automatically convert core code blocks to this new extended code block.
 		deprecated: [
-			...( settings.deprecated || [] ),
+			...(settings.deprecated || []),
 			{
 				attributes: {
 					...settings.attributes,
@@ -266,17 +263,17 @@ const extendCodeBlockWithSyntaxHighlighting = ( settings ) => {
 					},
 				},
 
-				save( { attributes } ) {
+				save({ attributes }) {
 					const className = attributes.language
 						? 'language-' + attributes.language
 						: '';
 					return (
 						<pre>
 							<code
-								lang={ attributes.language }
-								className={ className }
+								lang={attributes.language}
+								className={className}
 							>
-								{ attributes.content }
+								{attributes.content}
 							</code>
 						</pre>
 					);
