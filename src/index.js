a385c0431f4753ef33bea0d9af00bf957d529016
Weston Ruter <westonruter@google.com>
2023-02-21 16:39:27 -0800

    Remove obsolete Gutenberg compat code

--- src/index.js
+++ src/index.js
@@ -19,22 +19,15 @@ import {
 } from '@wordpress/components';
 import { Fragment, useRef, useEffect, useState } from '@wordpress/element';
 import {
-	__experimentalBlock as ExperimentalBlock, /* eslint-disable-line -- WP 5.5 */
-	useBlockProps, // GB 9.2, WP 5.6
-	PlainText,
+	useBlockProps,
 	RichText,
 	InspectorControls,
 } from '@wordpress/block-editor';
-import { escapeEditableHTML } from '@wordpress/escape-html';
 
 /**
  * Internal dependencies
  */
-import {
-	escape,
-	escapeIncludingEditableHTML,
-	escapeIncludingAmpersands,
-} from './utils';
+import { escape } from './utils';
 
 const languageNames = syntaxHighlightingCodeBlockLanguageNames;
 
@@ -56,15 +49,7 @@ const extendCodeBlockWithSyntaxHighlighting = (settings) => {
 
 		useEffect(() => {
 			if (textAreaRef.current !== null) {
-				let element = textAreaRef.current;
-
-				// In Gutenberg 7.8 and below, the DOM element was stored in a property with the name of the node type.
-				// In 7.9+, the DOM element is now stored in `current`. This block is here for backward-compatibility
-				// with older Gutenberg versions.
-				if (element.hasOwnProperty('textarea')) {
-					element = textAreaRef.current.textarea;
-				}
-
+				const element = textAreaRef.current;
 				const computedStyles = window.getComputedStyle(element);
 
 				setStyles({
@@ -78,44 +63,29 @@ const extendCodeBlockWithSyntaxHighlighting = (settings) => {
 			}
 		}, [props.style]);
 
-		const TextArea = useBlockProps ? RichText : PlainText;
-
-		if (useBlockProps) {
-			props.preserveWhiteSpace = true;
-			props.__unstablePastePlainText = true; // See https://github.com/WordPress/gutenberg/pull/27236
-		}
-
 		return (
 			<Fragment>
-				<TextArea ref={textAreaRef} {...props} />
+				<RichText ref={textAreaRef} {...props} />
 				<div
 					aria-hidden={true}
 					className="code-block-overlay"
 					style={styles}
 				>
-					{props.value.split(/\n/).map((v, i) => {
+					{(props.value || '').split(/\n/).map((v, i) => {
 						let cName = 'loc';
 
 						if (highlightedLines.has(i)) {
 							cName += ' highlighted';
 						}
 
-						if (useBlockProps) {
-							return (
-								<span
-									key={i}
-									className={cName}
-									dangerouslySetInnerHTML={{
-										__html: v || ' ',
-									}}
-								/>
-							);
-						}
-
 						return (
-							<span key={i} className={cName}>
-								{v || ' '}
-							</span>
+							<span
+								key={i}
+								className={cName}
+								dangerouslySetInnerHTML={{
+									__html: v || ' ',
+								}}
+							/>
 						);
 					})}
 				</div>
@@ -170,7 +140,9 @@ const extendCodeBlockWithSyntaxHighlighting = (settings) => {
 			...syntaxHighlightingCodeBlockType.attributes, // @todo Why can't this be supplied via a blocks.getBlockAttributes filter?
 		},
 
-		edit({ attributes, setAttributes, className }) {
+		edit({ attributes, setAttributes, onRemove }) {
+			const blockProps = useBlockProps();
+
 			const updateLanguage = (language) => {
 				setAttributes({ language });
 			};
@@ -195,18 +167,32 @@ const extendCodeBlockWithSyntaxHighlighting = (settings) => {
 				(languageOption) => languageOption.label.toLowerCase()
 			);
 
-			const textAreaProps = {
-				value: attributes.content || '',
-				highlightedLines: parseHighlightedLines(
-					attributes.highlightedLines
-				),
-				onChange: (content) => setAttributes({ content }),
-				placeholder: __('Write code…'),
-				'aria-label': __('Code'),
-				className: [
-					'shcb-textedit',
-					attributes.wrapLines ? 'shcb-textedit-wrap-lines' : '',
-				].join(' '),
+			const richTextProps = {
+				// These RichText props must mirror core <https://github.com/WordPress/gutenberg/blob/a42fd75/packages/block-library/src/code/edit.js#L12-L19>.
+				...{
+					tagName: 'code',
+					value: attributes.content,
+					onChange: (content) => setAttributes({ content }),
+					onRemove,
+					placeholder: __('Write code…'),
+					'aria-label': __('Code'),
+					preserveWhiteSpace: true,
+					__unstablePastePlainText: true, // See <https://github.com/WordPress/gutenberg/pull/27236>.
+				},
+
+				// Additional props unique to HighlightableTextArea.
+				...{
+					highlightedLines: parseHighlightedLines(
+						attributes.highlightedLines
+					),
+					className: [
+						'shcb-textedit',
+						attributes.wrapLines ? 'shcb-textedit-wrap-lines' : '',
+					].join(' '),
+
+					// Copy the styles to ensure that the code-block-overlay is updated when the font size is changed.
+					style: blockProps.style,
+				},
 			};
 
 			return (
@@ -275,75 +261,22 @@ const extendCodeBlockWithSyntaxHighlighting = (settings) => {
 							</PanelRow>
 						</PanelBody>
 					</InspectorControls>
-					{(() => {
-						if (useBlockProps) {
-							const blockProps = useBlockProps();
-
-							// Copy the styles to ensure that the code-block-overlay is updated when the font size is changed in Gutenberg 9.5+.
-							textAreaProps.style = blockProps.style;
-
-							// Must be kept in sync with Gutenberg 9.2+: <https://github.com/WordPress/gutenberg/blob/v9.2.0/packages/block-library/src/code/edit.js>.
-							return (
-								<pre {...blockProps}>
-									<HighlightableTextArea {...textAreaProps} />
-								</pre>
-							);
-						} else if (
-							// WP 5.5 required using a lightBlockWrapper, which was replaced by the blockProps above in WP 5.6.
-							settings.supports &&
-							settings.supports.lightBlockWrapper &&
-							ExperimentalBlock &&
-							ExperimentalBlock.pre
-						) {
-							// From Gutenberg 7.8...9.0: <https://github.com/WordPress/gutenberg/blob/v7.8.0/packages/block-library/src/code/edit.js>.
-							return (
-								<ExperimentalBlock.pre>
-									<HighlightableTextArea
-										{...textAreaProps}
-										__experimentalVersion={2}
-										tagName="code"
-									/>
-								</ExperimentalBlock.pre>
-							);
-						}
-
-						// For WordPress versions older than 5.5 (Gutenberg<7.8): <https://github.com/WordPress/gutenberg/blob/v7.7.0/packages/block-library/src/code/edit.js>.
-						return (
-							<div key="editor-wrapper" className={className}>
-								<HighlightableTextArea {...textAreaProps} />
-							</div>
-						);
-					})()}
+					{/* Keep in sync with https://github.com/WordPress/gutenberg/blob/a42fd75/packages/block-library/src/code/edit.js#L10-L21 */}
+					<pre {...blockProps}>
+						<HighlightableTextArea {...richTextProps} />
+					</pre>
 				</Fragment>
 			);
 		},
 
 		save({ attributes }) {
-			if (useBlockProps) {
-				// From Gutenberg v9.2+ (WordPress 5.6+): <https://github.com/WordPress/gutenberg/blob/v9.2.0/packages/block-library/src/code/save.js>.
-				return (
-					<pre {...useBlockProps.save()}>
-						<RichText.Content
-							tagName="code"
-							value={escape(attributes.content)}
-						/>
-					</pre>
-				);
-			} else if (escapeEditableHTML instanceof Function) {
-				// From Gutenberg v6.9.0 until v9.0.0 (WordPress 5.4 & 5.5): <https://github.com/WordPress/gutenberg/blob/v9.0.0/packages/block-library/src/code/save.js>.
-				return (
-					<pre>
-						<code>
-							{escapeIncludingEditableHTML(attributes.content)}
-						</code>
-					</pre>
-				);
-			}
-
-			// From Gutenberg v9.0 (WordPress 5.3) and before: <https://github.com/WordPress/gutenberg/blob/v9.0.0/packages/block-library/src/code/save.js>.
+			// Keep in sync with <https://github.com/WordPress/gutenberg/blob/a42fd75/packages/block-library/src/code/save.js#L13-L18>.
 			return (
-				<pre>
-					<code>{escapeIncludingAmpersands(attributes.content)}</code>
+				<pre {...useBlockProps.save()}>
+					<RichText.Content
+						tagName="code"
+						value={escape(attributes.content)}
+					/>
 				</pre>
 			);
 		},
