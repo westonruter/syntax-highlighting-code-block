e0a7d7f67f43e965538f8c42609da5afb174db4d
Weston Ruter <westonruter@google.com>
2023-04-07 15:39:37 -0700

    Work around PhpStorm not handling path correctly in PHPStan config

--- phpstan-baseline.php
+++ phpstan-baseline.php
@@ -4,31 +4,31 @@ $ignoreErrors   = [];
 $ignoreErrors[] = [
 	'message' => '#^Access to an undefined property WP_Block_Type\\:\\:\\$editor_script\\.$#',
 	'count'   => 1,
-	'path'    => __DIR__ . '/syntax-highlighting-code-block.php',
+	'path'    => '*', // Originally `__DIR__ . '/syntax-highlighting-code-block.php'`, but see https://youtrack.jetbrains.com/issue/WI-63891/PHPStan-ignoreErrors-configuration-isnt-working-with-inspections#focus=Comments-27-6188783.0-0
 ];
 $ignoreErrors[] = [
 	'message' => '#^Access to an undefined property WP_Block_Type\\:\\:\\$editor_style\\.$#',
 	'count'   => 1,
-	'path'    => __DIR__ . '/syntax-highlighting-code-block.php',
+	'path'    => '*',
 ];
 
 // See <https://github.com/phpstan/phpstan-src/pull/2277#issuecomment-1481954014>.
 $ignoreErrors[] = [
 	'message' => '#^Call to function property_exists\\(\\) with WP_Block_Type and \'editor_script…\' will always evaluate to true\\.$#',
 	'count'   => 1,
-	'path'    => __DIR__ . '/syntax-highlighting-code-block.php',
+	'path'    => '*',
 ];
 $ignoreErrors[] = [
 	'message' => '#^Call to function property_exists\\(\\) with WP_Block_Type and \'editor_style_handles\' will always evaluate to true\\.$#',
 	'count'   => 1,
-	'path'    => __DIR__ . '/syntax-highlighting-code-block.php',
+	'path'    => '*',
 ];
 
 // TODO: Figure out why this is being raised. It can't seem to be due to <https://github.com/phpstan/phpstan/issues/3770> since it's not a closure.
 $ignoreErrors[] = [
 	'message' => '#^Parameter \\#1 \\$autoload_function of function spl_autoload_register expects callable\\(string\\)\\: void, \'Highlight…\' given\\.$#',
 	'count' => 1,
-	'path' => __DIR__ . '/syntax-highlighting-code-block.php',
+	'path' => '*',
 ];
 
 return [ 'parameters' => [ 'ignoreErrors' => $ignoreErrors ] ];
