26f44b5f12030731897f0e5ffebef3d115e7dfee
Weston Ruter <westonruter@google.com>
2023-04-21 17:12:29 -0700

    Add script to symlink wp-env install-paths

---
+++ bin/symlink-wp-env-install-paths.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+install_path=$( npm run wp-env install-path 2>/dev/null | tail -n1)
+if [[ -z "$install_path" ]]; then
+	echo "Error: Unable to get install-path. Make sure you first did npm run wp-env start."
+	exit 1
+fi
+
+core_dir="wp-core"
+if [[ -e "$core_dir" ]]; then
+	rm "$core_dir"
+fi
+ln -s "$install_path/WordPress" "$core_dir"
+echo "Created $core_dir symlink"
+
+tests_dir="wp-tests-phpunit"
+if [[ -e "$tests_dir" ]]; then
+	rm "$tests_dir"
+fi
+ln -s "$install_path/WordPress-PHPUnit/tests/phpunit" "$tests_dir"
+echo "Created $tests_dir symlink"
