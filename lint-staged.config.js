a40686eca115e239305c0ef9462b4369a9ebc3cd
Weston Ruter <weston@ruter.net>
2025-11-22 14:34:32 -0800

    Remove redundant prettier dependency

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,8 +2,8 @@
  * @type {import('lint-staged', { with: { 'resolution-mode': 'import' } }).Configuration}
  */
 const config = {
-	'*.{js,ts,mjs}': [ 'npm run lint:js', () => 'npx tsc' ],
-	'*.css': [ 'npm run lint:css' ],
+	'*.{js,ts,mjs}': ['npm run lint:js', () => 'npx tsc'],
+	'*.css': ['npm run lint:css'],
 	'composer.{json,lock}': [
 		() => 'composer validate --strict',
 		() => 'composer normalize --dry-run',
@@ -13,7 +13,7 @@ const config = {
 		() => 'composer phpstan',
 		() => 'npm run verify-version-consistency',
 	],
-	'*.md': [ 'npm run lint:md', 'npm run verify-version-consistency' ],
+	'*.md': ['npm run lint:md', 'npm run verify-version-consistency'],
 	'README.md': [
 		() => 'npm run verify-version-consistency',
 		() => 'npm run transform-readme',
